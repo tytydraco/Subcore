829e7e3d4189e841b383e06c430b23d52c84db85
Tyler Nijmeh <tylernij@gmail.com>
2018-07-12 22:29:43 +0000

    misc: inline read and write files for faster performance
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- misc.h
+++ misc.h
@@ -3,8 +3,27 @@
 
 #include <string>
 namespace IO {
-	void write_file(std::string path, std::string in);
-	std::string read_file(std::string path);
+	inline void write_file(std::string path, std::string in) {
+		std::ofstream of;
+		of.open(path);
+
+		if (!of)
+			return;
+
+		of << in;
+		of.close();
+	}
+
+	inline std::string read_file(std::string path) {
+		std::ifstream ifs(path);
+
+		// return "" and not null to avoid runtime errors
+		if (!ifs)
+			return "";
+
+		return std::string((std::istreambuf_iterator<char>(ifs)),
+					(std::istreambuf_iterator<char>()));
+	}
 }
 
 #endif
