2bae992762a0130152be9859f840af611598e62a
Tyler Nijmeh <tylernij@gmail.com>
2018-07-26 16:53:38 -0700

    misc: move larger functions out of inline header
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- misc.h
+++ misc.h
@@ -2,31 +2,12 @@
 #define MISC_H
 
 #include <string>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 namespace IO {
-	inline void write_file(std::string path, std::string in) {
-		std::ofstream of;
-		of.open(path);
-
-		if (!of)
-			return;
-
-		of << in;
-		of.close();
-	}
-
-	inline std::string read_file(std::string path) {
-		std::ifstream ifs(path);
-
-		// return "" and not null to avoid runtime errors
-		if (!ifs)
-			return "";
-
-		return std::string((std::istreambuf_iterator<char>(ifs)),
-					(std::istreambuf_iterator<char>()));
-	}
+	void write_file(std::string path, std::string in);
+	std::string read_file(std::string path);
 
 	inline bool path_exists(std::string path) {
 		struct stat st;
